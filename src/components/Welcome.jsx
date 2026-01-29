5a5104d809cbf20913c03dab154666c82d20568b
Sarthak Gupta <sarthakgupta2503@example.com>
2026-01-29 18:51:40 +0530

    minor changes

---
+++ src/components/Welcome.jsx
@@ -0,0 +1,35 @@
+import {useRef } from "react";
+
+const renderText = (text , className , baseWeight = 400) =>{
+    return [ ... text].map((char,i) => (
+        <span key={i} className={className} style = {{fontVariationSettings : `wght ${baseWeight}`}}>
+            {char === " "? "\u00A0" : char}
+ 
+        </span>
+    ))
+}
+
+const Welcome = () => {
+  const titleRef = useRef(null);
+  const subtitleRef = useRef(null);
+
+  return <section id = "welcome">
+   <p ref = {subtitleRef}>
+    {renderText(
+        "Hey , I'm Sarthak ! Welcome to my" ,
+        "text-3xl font-georama" ,
+         100 )}</p>
+   <h1 ref={titleRef} className="mt-7">
+    {renderText("portfolio" , "text-9xl italic font-georama" )}
+     
+    </h1>
+
+    <div className="small-screen">
+        <p>This portfolio is designed for desktop/tablet screens only.</p>
+
+    </div>
+  </section>
+
+}
+
+export default Welcome;
---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+import Navbar from "#components/NavBar.jsx";
+import Welcome from "#components/Welcome.jsx";
+
+export {Navbar , Welcome};
